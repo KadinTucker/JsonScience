f715e1aca4b0d3f58308f02f833924ee3afe6465
Kxoe <kxoethecat@gmail.com>
2018-02-12 21:46:43 -0700

    Added assertions to jsonPlot

--- jsonPlot.py
+++ jsonPlot.py
@@ -22,13 +22,18 @@ def loadJson(filename):
     except:
         print('Failed to load ' + filename + '!')
         input()
-        return []
+        return
     return json.load(file)
 
+def determineValidityOfData(data):
+    assert type(data[0]) is str, "Missing plot title."
+    assert type(data[1]) is str, "Missing x-axis title."
+    assert type(data[3]) is str, "Missing y-axis title."
+    assert len(data[2]) == data[4], "X and Y value lists are not the same length."
+
 def main():
-    data = []
-    while len(data) == 0:
-        data = loadJson(input('Load what file? '))
+    data = loadJson(input('Load what file? '))
+    determineValidityOfData(data)
     mpl.pyplot.scatter(data[2], data[4])
     mpl.pyplot.xlabel(data[1])
     mpl.pyplot.ylabel(data[3])
