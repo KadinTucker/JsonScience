fc79bae08b0b08e5682e8aab9ca94d53c7d58e7d
Kxoe <kxoethecat@gmail.com>
2018-02-13 18:19:17 -0700

    Added basic boolean operator support for ErrVal

--- errorProp.py
+++ errorProp.py
@@ -24,3 +24,18 @@ class ErrVal():
     
     def __str__(self):
         return str(self.value) + " +/- " + str(self.error)
+    
+    def __eq__(self, other):
+        return self.value == other.value
+    
+    def __lt__(self, other):
+        return self.value < other.value
+    
+    def __le__(self, other):
+        return self.value <= other.value
+    
+    def __gt__(self, other):
+        return self.value > other.value
+    
+    def __ge__(self, other):
+        return self.value >= other.value
